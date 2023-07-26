ff27553e90608c09bd5e5e4a54aba231edd6f210
Siddharth Banga <91724657+VOSID8@users.noreply.github.com>
2023-07-26 18:47:25 +0530

    Kafka scripts updated

--- kafka-scripts/06-delete-topic.cmd
+++ kafka-scripts/06-delete-topic.cmd
@@ -1,1 +1,7 @@
 C:\kafka2\bin\windows\kafka-topics.bat --bootstrap-server localhost:9092 --delete --topic flinkex
+
+
+
+rmdir /s /q C:\tmp\kafka-logs
+rmdir /s /q C:\tmp\zookeeper
+rmdir /s /q C:\tmp\kraft-combined-logs
